1f31cbdf88d4eea159146202244db77a6361fec0
Gina Rico <gina.rico.llorca@gmail.com>
2023-09-14 12:38:34 +0200

    Added Spotify

--- spotify.sql
+++ spotify.sql
@@ -16,3 +16,90 @@ CREATE TABLE Usuaris (
     CONSTRAINT CHECK (sexe IN ('Hombre', 'Mujer', 'No Binario')),
     PRIMARY KEY (id)
 );
+
+CREATE TABLE Targetes (
+	id INT NOT NULL AUTO_INCREMENT,
+    numero VARCHAR(16),
+	caducitat VARCHAR(5),
+    CVV VARCHAR(3),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Paypal (
+	id INT NOT NULL AUTO_INCREMENT,
+    usuari VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Subscripcions (
+	id INT NOT NULL AUTO_INCREMENT,
+    dataInici DATE,
+    dataRenovacio DATE,
+    formaPagament VARCHAR(17),
+    idTargeta INT,
+    idPaypal INT,
+    CONSTRAINT CHECK (formaPagament IN('Paypal', 'Targeta de credit')),
+    PRIMARY KEY(id),
+    FOREIGN KEY (idTargeta) REFERENCES Targetes(id),
+    FOREIGN KEY (idPayPal) REFERENCES PayPal(id)
+);
+
+CREATE TABLE Pagaments (
+	numeroOrdre INT NOT NULL UNIQUE,
+    dataPagament DATE,
+    idUsuari INT NOT NULL,
+    total DECIMAL (10, 2),
+    PRIMARY KEY(numeroOrdre),
+    FOREIGN KEY (idUsuari) REFERENCES Usuaris(id)
+);
+
+CREATE TABLE Playlist (
+	id INT NOT NULL AUTO_INCREMENT,
+    titol VARCHAR(50),
+    quantitat INT,
+    dataCreacio DATE,
+    usuariPropietariID INT,
+    eliminada BOOLEAN,
+    dataEliminacio DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuariPropietariID) REFERENCES Usuaris(id)
+);
+
+CREATE TABLE Artista (
+	id INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(50),
+    imatge BLOB,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Album (
+	id INT NOT NULL AUTO_INCREMENT,
+    titol VARCHAR(50),
+    anyPublicacio YEAR,
+    portada BLOB,
+    artistaID INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (artistaID) REFERENCES Artista(id)
+);
+
+CREATE TABLE Cancons (
+	id INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(50),
+    durada INT,
+    reproduccions INT,
+    albumID INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (albumID) REFERENCES Album(id)
+);
+
+CREATE TABLE PlaylistDetall (
+    id INT NOT NULL AUTO_INCREMENT,
+    playlistID INT,
+    usuariID INT,
+    cancoID INT,
+    dataAfegida DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (playlistID) REFERENCES Playlist(id),
+    FOREIGN KEY (usuariID) REFERENCES Usuaris(id),
+    FOREIGN KEY (cancoID) REFERENCES Cancons(id)
+);
