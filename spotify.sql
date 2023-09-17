c405dc4ab88b937bb26378215f1915960e8f71a2
Gina Rico <gina.rico.llorca@gmail.com>
2023-09-17 11:58:54 +0200

    Spotify completado

--- spotify.sql
+++ spotify.sql
@@ -2,21 +2,6 @@ CREATE DATABASE IF NOT EXISTS Spotify;
 
 USE Spotify;
 
-CREATE TABLE Usuaris (
-	id INT NOT NULL AUTO_INCREMENT,
-    tipus VARCHAR(7),
-    email VARCHAR(20),
-    password VARCHAR(30),
-    usuari VARCHAR(30),
-    dataNeixament DATE,
-    sexe VARCHAR(10),
-    pais VARCHAR(20),
-    codi_postal VARCHAR(6),
-    CONSTRAINT CHECK (tipus IN ('free', 'premium')),
-    CONSTRAINT CHECK (sexe IN ('Hombre', 'Mujer', 'No Binario')),
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE Targetes (
 	id INT NOT NULL AUTO_INCREMENT,
     numero VARCHAR(16),
@@ -40,8 +25,28 @@ CREATE TABLE Subscripcions (
     idPaypal INT,
     CONSTRAINT CHECK (formaPagament IN('Paypal', 'Targeta de credit')),
     PRIMARY KEY(id),
+	UNIQUE KEY (idTargeta),
+	UNIQUE KEY (idPaypal),
     FOREIGN KEY (idTargeta) REFERENCES Targetes(id),
     FOREIGN KEY (idPayPal) REFERENCES PayPal(id)
+);
+
+CREATE TABLE Usuaris (
+	id INT NOT NULL AUTO_INCREMENT,
+    tipus VARCHAR(7),
+    email VARCHAR(20),
+    password VARCHAR(30),
+    usuari VARCHAR(30),
+    dataNaixement DATE,
+    sexe VARCHAR(10),
+    pais VARCHAR(20),
+    codi_postal VARCHAR(6),
+    idSubscripcio INT,
+    CONSTRAINT CHECK (tipus IN ('free', 'premium')),
+    CONSTRAINT CHECK (sexe IN ('Hombre', 'Mujer', 'No Binario')),
+    FOREIGN KEY (idSubscripcio) REFERENCES Subscripcions(id),
+    UNIQUE KEY (idSubscripcio),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Pagaments (
@@ -65,7 +70,7 @@ CREATE TABLE Playlist (
     FOREIGN KEY (usuariPropietariID) REFERENCES Usuaris(id)
 );
 
-CREATE TABLE Artista (
+CREATE TABLE Artistes (
 	id INT NOT NULL AUTO_INCREMENT,
     nom VARCHAR(50),
     imatge BLOB,
@@ -79,7 +84,7 @@ CREATE TABLE Album (
     portada BLOB,
     artistaID INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (artistaID) REFERENCES Artista(id)
+    FOREIGN KEY (artistaID) REFERENCES Artistes(id)
 );
 
 CREATE TABLE Cancons (
@@ -102,4 +107,44 @@ CREATE TABLE PlaylistDetall (
     FOREIGN KEY (playlistID) REFERENCES Playlist(id),
     FOREIGN KEY (usuariID) REFERENCES Usuaris(id),
     FOREIGN KEY (cancoID) REFERENCES Cancons(id)
+);
+
+CREATE TABLE Seguiments (
+	id INT NOT NULL AUTO_INCREMENT,
+    artistaID INT NOT NULL,
+    usuariID INT NOT NULL,
+    UNIQUE KEY (artistaID, usuariID),
+    PRIMARY KEY(id),
+    FOREIGN KEY (artistaID) REFERENCES Artistes(id),
+    FOREIGN KEY (usuariID) REFERENCES Usuaris(id)
+);
+
+CREATE TABLE ArtistesRelacionats (
+	id INT NOT NULL AUTO_INCREMENT,
+    artistaID INT NOT NULL,
+    artistaID2 INT NOT NULL,
+    UNIQUE KEY (artistaID, artistaID2),
+    PRIMARY KEY (id),
+    FOREIGN KEY (artistaID) REFERENCES Artistes(id),
+    FOREIGN KEY (artistaID2) REFERENCES Artistes(id)
+);
+
+CREATE TABLE AlbumsPreferits (
+	id INT NOT NULL AUTO_INCREMENT,
+    albumID INT NOT NULL,
+    usuariID INT NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (albumID, usuariID),
+    FOREIGN KEY (albumID) REFERENCES Album(id),
+    FOREIGN KEY (usuariID) REFERENCES Usuaris(id)
+);
+
+CREATE TABLE CanconsPreferides (
+	id INT NOT NULL AUTO_INCREMENT,
+    cancoID INT NOT NULL,
+    usuariID INT NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (cancoID, usuariID),
+    FOREIGN KEY (cancoID) REFERENCES Cancons(id),
+    FOREIGN KEY (usuariID) REFERENCES Usuaris(id)
 );
